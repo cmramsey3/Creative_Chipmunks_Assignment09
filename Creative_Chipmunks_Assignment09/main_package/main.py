6ca29b82907f8dbef660399a254b5e1883fc03f7
cmramsey3 <coltonramsey3@gmail.com>
2025-04-01 12:38:53 -0400

    Add project files.

---
+++ Creative_Chipmunks_Assignment09/main_package/main.py
@@ -0,0 +1,18 @@
+# File Name : main.py
+# Student Name: Colton Ramsey, Alisha Siddiqui, Daquan Daniels
+# email: ramseyc6@mail.uc.edu, siddiqas@mail.uc.edu, danieldu@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 04/03/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: will work in teams to develop a VS project that accesses our SQL Server
+# instance, extracts some data from the *Grocery Store Simulator database, and produces some
+# interesting results.
+
+# Brief Description of what this module does: Contains the main code that will be run to execute the instructions 
+# in the assignment.
+# Citations:
+# Anything else that's relevant:
+
+if __name__ == "__main__":
+    pass
