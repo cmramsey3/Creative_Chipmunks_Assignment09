e334c26318aaf3032bf571bd4ce7b2c92110a893
cmramsey3 <coltonramsey3@gmail.com>
2025-04-01 22:09:15 -0400

    Working results from the assignment. Needs updated Doc Strings

--- Creative_Chipmunks_Assignment09/dataManagementpackage/dataManagement.py
+++ Creative_Chipmunks_Assignment09/dataManagementpackage/dataManagement.py
@@ -1,48 +1,74 @@
+# File Name : main.py
+# Student Name: Colton Ramsey, Alisha Siddiqui, Daquan Daniels
+# email: ramseyc6@mail.uc.edu, siddiqas@mail.uc.edu, danieldu@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 04/03/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: will work in teams to develop a VS project that accesses our SQL Server
+# instance, extracts some data from the *Grocery Store Simulator database, and produces some
+# interesting results.
+
+# Brief Description of what this module does: Contains the main code that will be run to execute the instructions 
+# in the assignment.
+# Citations:
+# Anything else that's relevant:
+
 import pyodbc
-import random 
+import random
 
-def connect_to_database(self):
+class DatabaseAssignment:
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        
+
+    def get_connection(self):
+        """Establish and return a database connection."""
+        return pyodbc.connect(self.connection_string)
+
+    def fetch_products(self):
+        """Retrieve product data from tProduct."""
+        query = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID FROM tProduct"
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        data = cursor.fetchall()
+        conn.close()
+        return data
+
+    def get_manufacturer_name(self, manufacturer_id):
+        """Retrieve the manufacturer name based on ManufacturerID."""
+        query = f"SELECT Manufacturer FROM tManufacturer WHERE ManufacturerID = {manufacturer_id}"
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        manufacturer_name = cursor.fetchone()
+        conn.close()
+        return manufacturer_name[0] if manufacturer_name else "Unknown Manufacturer"
+
+    def get_brand_name(self, brand_id):
+        """Retrieve the brand name based on BrandID."""
+        query = f"SELECT Brand FROM tBrand WHERE BrandID = {brand_id}"
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        brand_name = cursor.fetchone()
+        conn.close()
+        return brand_name[0] if brand_name else "Unknown Brand"
+
+    def get_items_sold(self, product_id):
+        """Retrieve the total number of items sold for a given ProductID."""
+        query = f"""
+        SELECT SUM(dbo.tTransactionDetail.QtyOfProduct) AS NumberOfItemsSold
+        FROM dbo.tTransactionDetail 
+        INNER JOIN dbo.tTransaction 
+        ON dbo.tTransactionDetail.TransactionID = dbo.tTransaction.TransactionID 
+        WHERE (dbo.tTransaction.TransactionTypeID = 1) 
+        AND (dbo.tTransactionDetail.ProductID = {product_id})
         """
-        Connect to our SQL Server instance
-        @return connection object
-        """
-        conn = pyodbc.connect(
-            'Driver={SQL Server};'
-            'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
-            'Database=GroceryStoreSimulator;'
-            'uid=IS4010Login;'
-            'pwd=P@ssword2;'
-        )
-        return conn
-
-class ProductRepository:#Daquan
-    """Handles retrieving product data from the database."""
-    
-    def __init__(self, db_connector):
-        self.db = db_connector
-
-class ProductService: #Daquan
-    """Handles business logic related to product selection."""
-    
-    def __init__(self, product_repo):
-        self.product_repo = product_repo
-
-class ManufacturerRepository: #Daquan
-    """Handles retrieving manufacturer information."""
-    
-    def __init__(self, db_connector):
-        self.db = db_connector
-
-class ManufacturerService: #Questiom 4
-    """Handles business logic related to manufacturer retrieval."""
-
-
-class BrandRepository: #Question 5
-    """Handles retrieving brand information."""
-
-
-class SalesRepository: #Question 6
-    """Handles retrieving sales data."""
-
-class SentenceService: #Question 7 
-    """Handles constructing the final output sentence.""" 
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        items_sold = cursor.fetchone()
+        conn.close()
+        return items_sold[0] if items_sold else 0
--- Creative_Chipmunks_Assignment09/main_package/main.py
+++ Creative_Chipmunks_Assignment09/main_package/main.py
@@ -14,6 +14,42 @@
 # Citations:
 # Anything else that's relevant:
 
+from dataManagementpackage.dataManagement import *
+import random
+
 if __name__ == "__main__":
-    pass
+
+    # Initialize the Class as an object
+    db_manager = DatabaseAssignment('Driver={SQL Server};'
+                                 'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                                 'Database=GroceryStoreSimulator;'
+                                 'uid=IS4010Login;'
+                                 'pwd=P@ssword2;'
+                                 ) # Given Database Doc String
+
+    # Step 1: Fetch product data
+    products = db_manager.fetch_products()
+
+    # Step 2: Randomly select one product
+    selected_product = random.choice(products)
+
+    # Store variables
+    product_id = selected_product[0]
+    description = selected_product[2]
+    manufacturer_id = selected_product[3]
+    brand_id = selected_product[4]
+
+    # Step 3-4: Get manufacturer name
+    manufacturer_name = db_manager.get_manufacturer_name(manufacturer_id)
+
+    # Step 5: Get brand name
+    brand_name = db_manager.get_brand_name(brand_id)
+
+    # Step 6: Get number of items sold
+    items_sold = db_manager.get_items_sold(product_id)
+
+    # Step 7: Construct and print the output sentence
+    output_sentence = f"The product '{description}' from {manufacturer_name} under the {brand_name} brand has sold {items_sold} units."
+    print(output_sentence)
+
 
